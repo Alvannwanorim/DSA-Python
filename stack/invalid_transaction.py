9d6b5b6b6f353fe7187b6a03e2588fdf9dbbe023
alvannwanorim <alvannwanorim@gmail.com>
2022-09-16 21:53:12 +0100

    Invalid Transactions - Stack

--- stack/invalid_transaction.py
+++ stack/invalid_transaction.py
@@ -7,13 +7,14 @@ def invalidTransaction(transactions):
         record[1] = int(record[1])
         record[2] = int(record[2])
         records.append(record)
-        
+
     for rec in records:
         if rec[2] > 1000:
             rec[1] = str(rec[1])
             rec[2] = str(rec[2])
             result.append(",".join(rec))
             continue
+        
         for t in records:
             if rec[0] == t[0] and abs(rec[1] - int(t[1])) <= 60 and rec[3] != t[3]:
                 rec[1] = str(rec[1])
