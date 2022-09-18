22485723d881e813802a535628455df9113ee1c8
alvannwanorim <alvannwanorim@gmail.com>
2022-09-18 21:26:28 +0100

    Meeting Room I- Stack

--- stack/meeting_room.py
+++ stack/meeting_room.py
@@ -1,2 +1,13 @@
-def meetingRoom(intervals):
-    return 
+class Solution(object):
+    def canAttendMeetings(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: bool
+        """
+        intervals.sort(key = lambda i: i[0])
+        print(intervals)
+        s = 0       
+        for i in range(len(intervals) - 1):
+            if intervals[i][1] > intervals[i + 1][0]:
+                return False
+        return True
