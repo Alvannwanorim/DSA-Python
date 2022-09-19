d982a98712370f5bec760f3b6c0bdae1f1893022
alvannwanorim <alvannwanorim@gmail.com>
2022-09-19 23:35:25 +0100

    .

--- stack/largest_rect.py
+++ stack/largest_rect.py
@@ -1,13 +1,16 @@
 def largest_rect(heights:list):
+    
     stack = []
     maxArea =0
     for i,h in enumerate(heights):
         newIndex =i 
         while stack and stack[-1][1] > h:
+
             index, height =stack.pop()
             maxArea= max(maxArea, height * (i - index))
             newIndex = index
         stack.append((newIndex,h))
+
     print(stack)
     for i,h in stack:
         currArea = h* (len(heights) - i)
