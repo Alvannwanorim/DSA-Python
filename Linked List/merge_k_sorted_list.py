8a4132c376f8ca7cbad3ab5b9467f032e0e552e6
alvannwanorim <alvannwanorim@gmail.com>
2022-09-21 16:33:41 +0100

    Feat: Merge K sorted lists = Linked List

--- Linked List/merge_k_sorted_list.py
+++ Linked List/merge_k_sorted_list.py
@@ -19,17 +19,18 @@ class Slution:
                 l2  = lists[i + 1 ] if (i + 1) < len(lists) else None
                 mergedLists.append(self.mergeLists(l1,l2))
             lists = mergedLists
-            return lists[0]
+        return lists[0]
     def mergeLists(self, l1, l2):
         dummy = ListNode()
         tail = dummy
         while l1 and l2:
-            if l1.val > l2.val:
+            if l1.val < l2.val:
                 tail.next = l1
                 l1 = l1.next 
+
             else:
                 tail.next = l2
-                l2 = l1.next 
+                l2 = l2.next 
             tail = tail.next
         if l1:
             tail.next = l1
