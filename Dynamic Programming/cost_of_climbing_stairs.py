a79e1bb3e42509379813b6ae377d4a57a36395d8
alvannwanorim <alvannwanorim@gmail.com>
2022-09-24 13:00:16 +0100

    Cost Climbing Stairs - Dynamic Programming

---
+++ Dynamic Programming/cost_of_climbing_stairs.py
@@ -0,0 +1,12 @@
+
+from cmath import cos
+from typing import List
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cost.append(0)
+
+        for i in range(len(cost)-3, -1, -1):
+            cost[i] += min(cost[i + 1], cost[i + 2])
+        return min(cost[0], cos[1])
