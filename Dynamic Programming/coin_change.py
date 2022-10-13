11e91c14af15bca53539d6dce23e79fce6dbb024
alvannwanorim <alvannwanorim@gmail.com>
2022-10-13 05:02:43 +0100

    leetcode - Underground system

--- Dynamic Programming/coin_change.py
+++ Dynamic Programming/coin_change.py
@@ -5,9 +5,13 @@ class Solution:
     def coinChange(self, coin: List[int], amount: int) -> int:
         dp = [amount +1 ] * (amount + 1 )
         dp[0] = 0
-
+       
         for a in range(1, amount +1 ):
             for c in coin:
                 if a - c >= 0:
                     dp[a] = min(dp[a], 1 + dp[a -c ])
+        print(dp)
         return dp[amount] if dp[amount] != amount +1 else -1
+        
+solution = Solution()
+print(solution.coinChange([2,3,4,5],6))
---
+++ leetcode/underground_drainage.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+
+class UndergroundSystem:
+
+    def __init__(self):
+        self.inTime = defaultdict(tuple)
+        self.outTime = defaultdict(tuple)
+
+    def checkIn(self, id: int, stationName: str, t: int) -> None:
+        self.inTime[id] = (t,stationName)
+
+    def checkOut(self, id: int, stationName: str, t: int) -> None:
+        startTime, startStation = self.inTime[id]
+        totalDifference = t- startTime
+        self.outTime[(startTime, startStation)].append(totalDifference)
+
+    def getAverageTime(self, startStation: str, endStation: str) -> float:
+        return sum(self.o[(startStation,endStation)])/len(self.o[(startStation,endStation)])
