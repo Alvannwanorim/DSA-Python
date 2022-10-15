de1890ed19da808abe10163f74e12879f525ab96
alvannwanorim <alvannwanorim@gmail.com>
2022-10-15 13:57:43 +0100

    Walls and Gates - Graph

--- Graph/walls_and_gates.py
+++ Graph/walls_and_gates.py
@@ -10,6 +10,17 @@ class Solution:
 
         visited = set()
         q = deque()
+
+        def addRoom(r,c):
+            if(r < 0 or c < 0 
+                or r == ROWS 
+                or c == COLS 
+                or (r, c) in visited 
+                or rooms[r][c] == -1):
+                return
+            visited.add((r,c))
+            q.append([r, c])
+            
         # loop through the rooms and append all the empty rooms to the queue
         for r in range(ROWS):
             for c in range(COLS):
@@ -32,12 +43,9 @@ class Solution:
                 addRoom(r, c + 1)
                 addRoom(r, c - 1)
             dist += 1
-        def addRoom(r,c):
-            if(r < 0 or c < 0 
-                or r == ROWS 
-                or c == COLS 
-                or (r, c) in visited 
-                or rooms[r][c] == -1):
-                return
-            visited.add(r,c)
-            q.append([r, c])
+        
+        return rooms
+
+s = Solution()
+
+print(s.awall_and_gates([[2147483647,-1,0,2147483647],[2147483647,2147483647,2147483647,-1]]))
