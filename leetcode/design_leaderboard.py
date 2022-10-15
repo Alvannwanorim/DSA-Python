6353b675c5de8cfedad4f20e52c9399ddc9446eb
alvannwanorim <alvannwanorim@gmail.com>
2022-10-15 14:29:52 +0100

    Leetcode - Design Leaderboard

---
+++ leetcode/design_leaderboard.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+
+
+class Leaderboard:
+
+    def __init__(self):
+        self.board = defaultdict()
+
+    def addScore(self, playerId: int, score: int) -> None:
+        if playerId not in self.board:
+            self.board[playerId] = 0
+        self.board[playerId] += score
+
+    def top(self, K: int) -> int:
+        values = [v for _,v in sorted(self.board.items(), keys=lambda item: item[1])]
+        values.sort(reverse= True)
+        total, i = 0, 0
+        while i < K:
+            total += values[i]
+            i += 1
+        return total
+    def reset(self, playerId: int) -> None:
+        self.board[playerId] = 0
