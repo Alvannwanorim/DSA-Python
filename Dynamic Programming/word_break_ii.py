7bd5862b38e5d1ebc3ee6901809467b0c54804e8
alvannwanorim <alvannwanorim@gmail.com>
2022-10-16 15:50:02 +0100

    Word Break II - Dynamic Programming

--- Dynamic Programming/word_break_ii.py
+++ Dynamic Programming/word_break_ii.py
@@ -17,8 +17,28 @@ class Solution:
                 if word in wordSet:
                     for subsentence in dp[startIndex]:
                         sublist.append((subsentence + ' ' + word).strip())
+                        print(sublist)
             dp[endIndex] = sublist
 
         return dp[len(s)]
 sol = Solution()
 print(sol.wordBreak('abcdef', [ 'ab', 'abc', 'cd', 'def', 'abcd', 'ef', 'c' ]))
+
+class Soultion2:
+    def wordBreak(self, s: str, wordDict: List[str]) ->List[str]:
+        if set(Counter(s).keys()) > set(Counter("".join(wordDict)).keys()):
+            return []
+        wordSet = set(wordDict)
+        dp = [[]] * (len(s)+ 1)
+        dp[0] =[""]
+
+        for endIndex in range(1,len(s) + 1):
+            sublist = []
+            for startIndex in range(0, endIndex):
+                word = s[startIndex:endIndex]
+                if word in wordSet:
+                    for subsentence in dp[startIndex]:
+                        sublist.append((subsentence + ' ' + word).strip())
+            dp[endIndex] = sublist
+
+        return dp[len(s)]
