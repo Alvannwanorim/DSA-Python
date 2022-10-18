7234fd1be04387e7298994f618b97b9ed6c6395a
alvannwanorim <alvannwanorim@gmail.com>
2022-10-18 21:32:47 +0100

    updates

--- Dynamic Programming/word_break_ii.py
+++ Dynamic Programming/word_break_ii.py
@@ -3,23 +3,26 @@ from typing import Counter, List
 
 
 class Solution:
-    def wordBreak(self, s: str, wordDict: List[str]) ->List[str]:
-        if set(Counter(s).keys()) > set(Counter("".join(wordDict)).keys()):
-            return []
-        wordSet = set(wordDict)
-        dp = [[]] * (len(s)+ 1)
-        dp[0] =[""]
-        for endIndex in range(1,len(s) + 1):
-            sublist = []
-            for startIndex in range(0, endIndex):
-                word = s[startIndex:endIndex]
-                if word in wordSet:
-                    for subsentence in dp[startIndex]:
-                        sublist.append((subsentence + ' ' + word).strip())
-                        print(startIndex, endIndex,sublist)
-            dp[endIndex] = sublist
-
-        return dp[len(s)]
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        memo = {}
+        def dfs(s):
+            print(s)
+            if s in memo:
+                return memo[s]
+            
+            res = []
+            for x in wordDict:
+                if s.startswith(x):
+                    if s == x:
+                        res += [x]
+                    else:
+                        for y in dfs(s[len(x):]):
+                            res += [x + ' ' + y]
+            memo[s] = res
+            print(memo)
+            return res
+                        
+        return dfs(s)
 sol = Solution()
 print(sol.wordBreak('catsanddog', ["cat","cats","and","sand","dog"]))
 
