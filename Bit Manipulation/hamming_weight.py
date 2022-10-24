a20757a6cfb7262d4841b04525dd9ea6a818e0a2
alvannwanorim <alvannwanorim@gmail.com>
2022-10-24 23:07:21 +0100

    Hamming Weight - Bit Manipilation

---
+++ Bit Manipulation/hamming_weight.py
@@ -0,0 +1,19 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res = 0 
+        while n:
+            res += n % 2
+            n = n >> 1
+        return res
+
+
+class Solution2:
+    def hammingWeight(self, n: int) -> int:
+        res = 0 
+        while n:
+            n &= (n - 1)
+            res += 1
+        return res
+
+sol = Solution2()
+print(sol.hammingWeight(0O100110000))
