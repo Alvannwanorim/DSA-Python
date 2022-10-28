2b0f2395466af3d66b4a6c891d6d7882f2948f3c
alvannwanorim <alvannwanorim@gmail.com>
2022-10-28 17:36:19 +0100

    Plus One - Math and Geometry

---
+++ Math and Geometry/plus_one.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        one = 1
+        i = 0
+        digits = digits[::-1]
+
+        while one:
+            if i <len(digits):
+                if digits[i] == 9:
+                    digits[i] = 1
+                else:
+                    digits[i] +=1
+                    one = 0
+            else:
+                digits.append(one)
+                one = 0
+            i += 1
+        return digits[::-1]
+sol = Solution()
+print(sol.plusOne([9,9,9]))
