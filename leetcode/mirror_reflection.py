2cc03d85daaf0936bc8dc039ea268b2c9923bb8f
alvannwanorim <alvannwanorim@gmail.com>
2022-10-31 20:26:34 +0100

    Mirror Reflection - Leetcode

--- leetcode/mirror_reflection.py
+++ leetcode/mirror_reflection.py
@@ -1,4 +1,4 @@
-from math import lcm
+from math import gcd, lcm
 
 
 class Solution:
@@ -10,5 +10,17 @@ class Solution:
         
         return (l//p)%2
 
-sol = Solution()
-print(sol.mirrorReflection(3,1))
+
+
+class Solution2:
+    def mirrorReflection(self, p: int, q: int):
+        GCD = gcd(p,q)
+
+        a , b = p // GCD , q // GCD
+        print(GCD, a, b)
+        if a % 2 == 0:
+            return 2
+        return 0 if b % 2 == 0 else 1
+
+sol = Solution2()
+print(sol.mirrorReflection(2,1))
