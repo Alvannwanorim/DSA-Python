d616fd29246c8beade08854695a2f74c7ecf9af0
alvannwanorim <alvannwanorim@gmail.com>
2022-11-03 13:10:25 +0100

    Kth Largest Element in Stream - Heap

---
+++ heap/kth_largest_element_in_stream.py
@@ -0,0 +1,25 @@
+import heapq
+from mimetypes import init
+from typing import List
+
+
+class KthLargest:
+    def __init__(self, k: init, nums: List[int]) -> None:
+        self.minHeap, self.k = nums, k
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+    
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
+
+kthLargest =KthLargest(3,[4, 5, 8, 2])
+print(kthLargest.add(3))
+print(kthLargest.add(5))
+print(kthLargest.add(10))
+print(kthLargest.add(9))
+print(kthLargest.add(4))
+
