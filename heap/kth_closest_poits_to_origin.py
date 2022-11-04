0c01850388f0b1c48b3efdc076d6d1e557234ba9
alvannwanorim <alvannwanorim@gmail.com>
2022-11-04 13:31:53 +0100

    Kth Closest Point to Origin - Heap

---
+++ heap/kth_closest_poits_to_origin.py
@@ -0,0 +1,17 @@
+import heapq
+from typing import List
+
+
+class Solution:
+    def kthClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        pts = []
+        for x, y in points:
+            dist = (abs((x -0) ** 2) + abs((y -0) ** 2))
+            pts.append([dist, x, y])
+
+            res = []
+            heapq.heapify(pts)
+            for i in range(k):
+                dist, x, y = heapq.heappop(pts)
+                res.append([x,y])
+            return res
