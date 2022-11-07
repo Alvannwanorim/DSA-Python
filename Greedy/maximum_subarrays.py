b3430273ab41791944e28b5cdc1288450e4a05d7
alvannwanorim <alvannwanorim@gmail.com>
2022-11-07 18:20:32 +0100

    Maximum sub array - Gready

---
+++ Greedy/maximum_subarrays.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSub = nums[0]
+        curSum = 0
+
+        for n in nums:
+            if curSum < 0:
+                curSum = 0
+            curSum += n
+            maxSub = max(maxSub, curSum)
+        return maxSub
+sol = Solution()
+print(sol.maxSubArray([-1,0,2,3,-1,-2,]))
