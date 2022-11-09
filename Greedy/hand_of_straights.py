19dc0f072870bfd146f9bf184f5b2e99e1346be2
alvannwanorim <alvannwanorim@gmail.com>
2022-11-09 21:47:54 +0100

    Hands of Straight - Greedy

---
+++ Greedy/hand_of_straights.py
@@ -0,0 +1,31 @@
+import heapq
+from typing import List
+
+
+class Solution:
+    def handOfStraights(self, hand: List[int], groupSize: int):
+        if len(hand) % groupSize:
+            return False
+        
+        count ={}
+        for n in hand:
+            count[n] = 1 + count.get(n,0)
+        
+        minH = list(count.keys())
+        heapq.heapify(minH)
+
+        while minH:
+            first = minH[0]
+
+            for i in range(first, first + groupSize):
+                if i not in count:
+                    return False
+                count[i] -= 1
+
+                if count[i] == 0:
+                    if i != minH[0]:
+                        return False
+                    heapq.heappop(minH)
+        return True
+sol =Solution()
+print(sol.handOfStraights([1,2,3,6,2,3,4,7,8], 3))
