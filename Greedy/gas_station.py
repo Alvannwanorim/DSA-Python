a1cc5e60ea1ed6efd97fbd1cfbac7c53abfc80e7
alvannwanorim <alvannwanorim@gmail.com>
2022-11-09 14:06:55 +0100

    Gas Station - Greedy

---
+++ Greedy/gas_station.py
@@ -0,0 +1,20 @@
+from cmath import cos
+from typing import List
+
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        start, end = len(gas) - 1, 0
+        total = gas[start] - cost[start]
+
+        while start >= end:
+            while total < 0 and start >= end:
+                start -= 1
+                total += gas[start] - cost[start]
+            if start == end:
+                return start
+            total += gas[end] - cost[end]
+            end += 1
+        return -1
+sol = Solution()
+print(sol.canCompleteCircuit([1,2,3,4,5,], [3,4,5,1,2]))
