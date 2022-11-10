b84beeba56d77901105add1668c71d3d4563ace2
alvannwanorim <alvannwanorim@gmail.com>
2022-11-10 19:32:23 +0100

    Merge Triplets - Gready

---
+++ Greedy/merge_triplets.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        good = set()
+
+        for t in triplets:
+            if t[0] > target[0] or t[1] > target[1] or t[2] > target[2]:
+                continue
+            for i, v in enumerate(t):
+                if v == target[i]:
+                    good.add(i)
+        return len(good) == 3
+
+sol = Solution()
+print(sol.mergeTriplets([[2,5,3],[1,8,4],[1,7,5]], [2,7,5]))
