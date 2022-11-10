acc2e29d47fd2d2f595eca431a16a194982c4cc5
alvannwanorim <alvannwanorim@gmail.com>
2022-11-10 20:11:22 +0100

    Partition Labels- Greedy

---
+++ Greedy/partition_labels.py
@@ -0,0 +1,21 @@
+class Solution:
+    def partitionLabels(self, s: str) -> list[int]:
+        lastIndex = {}
+
+        for i, c in enumerate(s):
+            lastIndex[c] = i
+        
+
+        res = []
+        size, end = 0, 0
+
+        for i, c in enumerate(s):
+            size += 1
+            end = max(end, lastIndex[c])
+
+            if i == end:
+                res.append(size)
+                size = 0
+        return res
+sol = Solution()
+print(sol.partitionLabels("abdbebooathkyymnytlllvv"))
