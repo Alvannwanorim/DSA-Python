d453be08ce9cc4581303446b8aa999c11040edc6
alvannwanorim <alvannwanorim@gmail.com>
2022-11-17 18:43:56 +0100

    Network Delay - Graph

--- Graph/network_delay.py
+++ Graph/network_delay.py
@@ -7,20 +7,22 @@ class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         edges = collections.defaultdict(list)
         for u, v, w in times:
-            edges[u].append((v,w))
+            edges[u].append([v, w])
         
         minHeap = [(0, k)]
-        visited = set()
         t = 0
+        visit = set()
+
         while minHeap:
             w1, n1 = heapq.heappop(minHeap)
-            if n1 in visited:
-                continue
-            visited.add(n1)
             t = max(t, w1)
-
+            if n1 in visit:
+                continue
+            visit.add(n1)
             for n2, w2 in edges[n1]:
-                if n2 not in visited:
-                    heapq.heappush(minHeap, (w1 + w2, n2))
+                if n2 not in visit:
+                    heapq.heappush(minHeap,(w1 + w2, n2))
         
-        return t if len(visited) == n else -1
+        return t if len(visit) == n else -1
+sol = Solution()
+print(sol.networkDelayTime([[2,1,1],[2,3,1],[3,4,1]], 4, 2))
