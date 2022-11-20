08fb4608118788768dc79c6e1a1878a0ec250b58
alvannwanorim <alvannwanorim@gmail.com>
2022-11-20 15:26:08 +0100

    Implement Trie - Trie

---
+++ Trie/implement_trie.py
@@ -0,0 +1,35 @@
+class TrieNode:
+    def __init__(self) -> None:
+        self.children = {}
+        self.endOfWord = False
+
+
+class Trie:
+    def __init__(self) -> None:
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.endOfWord = True
+    def search(self, word: str) -> bool:
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return cur.endOfWord
+
+    def startWith(self, prefix: str) -> bool:
+        cur = self.root
+
+        for c in prefix:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return True
