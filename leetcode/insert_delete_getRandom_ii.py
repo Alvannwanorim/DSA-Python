f2dea84042668c34203b2808b1a66f65d25094fa
alvannwanorim <alvannwanorim@gmail.com>
2022-11-23 05:38:38 +0100

    Insert Delet and GetRandom - Leetcode

--- leetcode/insert_delete_getRandom_ii.py
+++ leetcode/insert_delete_getRandom_ii.py
@@ -6,14 +6,31 @@ class RandomizedCollection:
 
     def __init__(self):
         self.numList = []
-        self.hasMap = defaultdict(list)
+        self.hasMap = defaultdict(set)
 
     def insert(self, val: int) -> bool:
-       return True
+        res = val in self.hasMap
+        indx = len(self.numList)
+        self.hasMap[val].add(indx)
+        self.numList.append(val)
+        return res
         
 
     def remove(self, val: int) -> bool:
-       return False
+        if not self.hasMap[val]:
+            return False
+        index = self.hasMap[val].pop()
+        last_val = self.numList[-1]
+        self.numList[index] = last_val
+        self.hasMap[last_val].add(index)
+        self.hasMap[last_val].remove(len(self.numList) -1)
+        
+        if len(self.hasMap[val]) == 0:
+            del self.hasMap[val]
+        self.numList.pop()
+        print(self.hasMap)
+        return True
+    
 
     def getRandom(self) -> int:
         return random.choice(self.numList)
@@ -21,8 +38,8 @@ class RandomizedCollection:
 rad = RandomizedCollection()
 print(rad.insert(1))
 print(rad.insert(2))
-print(rad.insert(3))
-print(rad.getRandom())
-print(rad.getRandom())
+print(rad.insert(2))
+# print(rad.getRandom())
+# print(rad.getRandom())
 print(rad.remove(1))
 print(rad.getRandom())
