b36e2215ba4331f531f0c19c9e415c5d2c7e09d0
alvannwanorim <alvannwanorim@gmail.com>
2022-11-24 17:20:54 +0100

    Top k frequent elements

---
+++ Arrays and HashMaps/top_k_frequent_elements.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def frequency(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            count[num] = 1 + count.get(num,0)
+        
+        for n, c in count.items():
+            freq[c].append(n)
+        res = []
+        print(count)
+        print(freq)
+        for i in range(len(freq) -  1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+sol = Solution()
+print(sol.frequency([1,2,2,3,4,4], 2))
