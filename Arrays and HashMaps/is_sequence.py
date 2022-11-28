bfe462c92cac96ca2601e86c81466c31a31a6ac9
alvannwanorim <alvannwanorim@gmail.com>
2022-11-28 23:08:32 +0100

    Is subSequence - Array

--- Arrays and HashMaps/is_sequence.py
+++ Arrays and HashMaps/is_sequence.py
@@ -2,11 +2,11 @@ class Solution:
     def isSequence(self, s: str, t: str) -> bool:
         i, j = 0, 0
 
-        while i <= len(s) and j <= len(t):
+        while i < len(s) and j < len(t):
             if s[i] == t[j]:
-                i +=1
-            j += 1
+                j +=1
+            i += 1
         return j == len(t)
 
-        
-
+sol = Solution()
+print(sol.isSequence("abcded", "acx"))
