3d8d0421b658d2b8a9c9ce1015ba778ffcb94c07
alvannwanorim <alvannwanorim@gmail.com>
2022-12-01 19:21:31 +0100

    Valid Sudoku - Array

---
+++ Arrays and HashMaps/valid_sudoku.py
@@ -0,0 +1,34 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def validSudoku(self, board: List[List[int]]) -> bool:
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (
+                    board[r][c] in rows[r] 
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r//3, c//3)]):
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r//3, c//3)].add(board[r][c])
+        
+        return True
+sol = Solution()
+print(sol.validSudoku([["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]))
