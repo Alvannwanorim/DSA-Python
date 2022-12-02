5f37b43cd293f529ab6443406676c3397a65cfce
alvannwanorim <alvannwanorim@gmail.com>
2022-12-02 07:40:48 +0100

    Encode Decode Strings - Arrays

---
+++ Arrays and HashMaps/encode_and_decode_strings.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+
+        for s in strs:
+            res += str(len(s)) + "#" + s 
+        return res
+
+    def decode(self, s: str) -> List[str]:
+        res, i = [], 0
+        while i <len(s):
+            j = i 
+            while s[j] != "#":
+                j += 1
+                length = int(s[i:j])
+                print(j, length)
+                res.append(s[j + 1: j+ 1 + length])
+                i = j + 1 + length
+        return res
+
+sol = Solution()
+print(sol.encode(["Hello","World"]))
+print(sol.decode("5#Hello5#World"))
