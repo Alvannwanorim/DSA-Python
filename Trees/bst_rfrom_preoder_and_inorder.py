b06aa329d79bd6dc0b41c2bc04da36e8d8a30518
alvannwanorim <alvannwanorim@gmail.com>
2022-12-04 20:30:58 +0100

    BST From PreOrer and InOrder Traversal

--- Trees/bst_rfrom_preoder_and_inorder.py
+++ Trees/bst_rfrom_preoder_and_inorder.py
@@ -8,12 +8,12 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder or not inorder:
+    def buildTree(self, preOrder: List[int], inOrder: List[int]) -> Optional[TreeNode]:
+        if not preOrder or not inOrder:
             return None
         
-        root = TreeNode(preorder[0])
-        mid = inorder.index(preorder[0])
-        root.left = self.buildTree(preorder[1 : mid +1], inorder[:mid])
-        root.right = self.buildTree(preorder[mid + 1 :], inorder[mid + 1:])
+        root = TreeNode(preOrder[0])
+        mid = inOrder.index(preOrder[0])
+        root.left = self.buildTree(preOrder[1 : mid +1], inOrder[:mid])
+        root.right = self.buildTree(preOrder[mid + 1 :], inOrder[mid + 1:])
         return root
