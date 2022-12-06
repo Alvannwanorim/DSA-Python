ab5abdf565de500b6d83c0fcb50c822a305b5650
alvannwanorim <alvannwanorim@gmail.com>
2022-12-06 21:33:26 +0100

    Same Tree

---
+++ Trees/same_tree.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def sameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if p and q and p.val == q.val:
+            return self.sameTree(p.left, q.left) and self.sameTree(p.right, q.right)
+        else:
+            return False
+        
