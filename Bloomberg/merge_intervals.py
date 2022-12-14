f5110f965248bd029e6febd367031c1105a60ad1
alvannwanorim <alvannwanorim@gmail.com>
2022-12-14 23:31:55 +0100

    merge Intervals

--- Bloomberg/merge_intervals.py
+++ Bloomberg/merge_intervals.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 class Solution:
-    def mergeInterval(self, intervals: List[List[int]]) ->List[List[int]]:
+    def mergeIntervals(self, intervals: List[List[int]]) ->List[List[int]]:
         intervals.sort(key= lambda i: i[0])
 
         output = [intervals[0]]
@@ -15,3 +15,7 @@ class Solution:
                 output.append([start, end])
             
         return output
+
+
+sol = Solution()
+print(sol.mergeIntervals([[1,2],[2,5],[4,7],[8,10],[12,16]]))
