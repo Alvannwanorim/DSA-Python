65d1b5d85d4dae4f66c360f9b7e4fc98af1ad506
alvannwanorim <alvannwanorim@gmail.com>
2022-12-15 21:17:33 +0100

    LRU Cache

---
+++ Bloomberg/lru_cache_ii.py
@@ -0,0 +1,31 @@
+from collections import OrderedDict
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+        
+
+    def get(self, key: int) -> int:
+        if not key in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache[key]:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+
+        if len(self.cache) > self.cache:
+            self.cache.popitem(last = False)
+        
+        return f'Cached'
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
