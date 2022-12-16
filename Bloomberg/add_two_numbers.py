62c1bbbe5ef35bc7601432b32fe542f84c13faef
alvannwanorim <alvannwanorim@gmail.com>
2022-12-16 23:45:45 +0100

    Add Two Numbers

--- Bloomberg/add_two_numbers.py
+++ Bloomberg/add_two_numbers.py
@@ -4,7 +4,6 @@ class NodeList:
     def __init__(self, val) -> None:
         self.val = val 
         self.next = None
-        self.prev = None
 class Solution:
     def addTwoNumbers(self, l1: Optional[NodeList], l2: Optional[NodeList]) -> Optional[NodeList]:
         dummy = NodeList(0)
