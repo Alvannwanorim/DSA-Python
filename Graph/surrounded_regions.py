41fb1fb6b7f695b0b38553d11aea592687dd98fc
alvannwanorim <alvannwanorim@gmail.com>
2022-12-18 23:57:31 +0100

    Surrounded Region

--- Graph/surrounded_regions.py
+++ Graph/surrounded_regions.py
@@ -3,20 +3,20 @@ from typing import List
 
 class Solution:
     def surroundedRegion(self, board: List[List[int]]):
-        ROWS,COLS = len(board), len(board[0])
+        ROWS, COLS = len(board), len(board[0])
 
-        def capture(r, c):
-            if(r < 0 or c < 0 or r == ROWS or c == COLS or board[r][c] != "O"):
-                return 
+        def capture(r,c):
+            if( r < 0 or r == ROWS or c < 0 or c == COLS or board[r][c] != "O"):
+                return
             board[r][c] = "T"
             capture(r + 1, c)
-            capture(r - 1, c)
-            capture(r, c + 1)
-            capture(r, c- 1)
-        
+            capture(r - 1,c)
+            capture(r , c + 1)
+            capture(r, c - 1)
+
         for r in range(ROWS):
             for c in range(COLS):
-                if board[r][c] == "O" and (r in [0, ROWS - 1] or c in [0, COLS - 1]):
+                if (board[r][c] =="O" and (r in [0, ROWS - 1] or  c in [0, COLS - 1])):
                     capture(r,c)
         
         for r in range(ROWS):
@@ -26,9 +26,10 @@ class Solution:
         
         for r in range(ROWS):
             for c in range(COLS):
-                if board[r][c] == "T":
+                if board[r][c] =="T":
                     board[r][c] = "O"
-        print(board)
+        
         return
-solution = Solution()
-solution.surroundedRegion([["X","O","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]])
+
+sol = Solution()
+sol.surroundedRegion([["X","O","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]])
