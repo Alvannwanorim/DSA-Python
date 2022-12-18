cc84021f7bd94788aa68f7f09bb9805ebc3b49b2
alvannwanorim <alvannwanorim@gmail.com>
2022-12-18 21:45:27 +0100

    Clone Graph

--- Graph/clone_hraph.py
+++ Graph/clone_hraph.py
@@ -1,5 +1,5 @@
 class Node: 
-    def __init__(self, val = 0, neighbors=None) -> None:
+    def __init__(self, val=0, neighbors=None) -> None:
         self.val = val
         self.neighbors = neighbors
 
@@ -7,3 +7,15 @@ class Node:
 class Solution:
     def cloneGraph(self, node: Node) -> Node:
         oldToNew = {}
+
+        def dfs(node):
+            if node in oldToNew:
+                return oldToNew[node]
+            
+            copy = Node(node.val)
+
+            for nei in node.neighbors:
+                copy.neighbors.append(dfs(nei))
+            
+            return copy
+        return dfs(node)
