26ad7eb2c75dd3f58923ea741a00504de13db6d7
alvannwanorim <alvannwanorim@gmail.com>
2022-12-19 01:06:50 +0100

    Rotten Oranges

--- Graph/rotten_oranges.py
+++ Graph/rotten_oranges.py
@@ -4,39 +4,34 @@ from typing import List
 
 class Solution:
     def rottenOranges(self, grid: List[List[int]]):
-        # initialize a data storage for the postion of rotten oranges
+        time, fresh = 0, 0
+
         q = collections.deque()
 
-        fresh = 0
-        time = 0
-        # loop through the grid to fetch all rotten oranges
-        # and also record the fresh ones
-        ROWS,COLS = len(grid), len(grid[0])
-
-        for r in range(ROWS):
-            for c in range(COLS):
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
                 if grid[r][c] == 1:
                     fresh += 1
                 if grid[r][c] == 2:
                     q.append((r,c))
         
-        # Loop through the queue and mark neighbors or the rotten oranges as rotten
-        # decrease the number of fresh oranges and increase the time
-        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
         while q and fresh > 0:
+            r,c = q.popleft()
             for i in range(len(q)):
-                r, c = q.popleft()
-
                 for dr, dc in directions:
-                    row, col = r + dr, c + dc 
-
-                    if(row in range(len(grid)) 
-                        and col in range(len(grid[0])) 
-                        and grid[row][col] == 1):
-                        grid[row][col] = 2
-                        fresh -= 1
+                    row, col = r + dr, c + dc
+                    if (row < 0 or row ==len(grid) or col < 0 or col == len(grid[0]) or grid[row][col] == 2):
+                        continue
+                    grid[row][col] = 2
+                    q.append((row, col))
+                    fresh -= 1
+            
             time += 1
-        return time if fresh == 0 else -1
+        
+        return time if fresh == 0 else -1 
+sol = Solution()
+print(sol.rottenOranges([[2,1,1],[1,1,0],[0,1,1]]))
 
 
         
