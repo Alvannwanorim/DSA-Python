6c807fc883a7340373e43b45992535a5086ce451
alvannwanorim <alvannwanorim@gmail.com>
2022-12-19 10:33:15 +0100

    Walls and Gates

--- Graph/walls_and_gates.py
+++ Graph/walls_and_gates.py
@@ -3,47 +3,35 @@ from typing import List
 
 
 class Solution:
-    def awall_and_gates(self, rooms: List[List[int]]) -> None:
-        ROWS, COLS = len(rooms), len(rooms[0])
+    def wallsAndGates(self, rooms: List[List[int]]) -> None:
+        ROWS, COL = len(rooms), len(rooms[0])
 
-        visited = set()
+        visit = set()
         q = deque()
-
+        
         def addRoom(r,c):
-            if(r < 0 or c < 0 
-                or r == ROWS 
-                or c == COLS 
-                or (r, c) in visited 
-                or rooms[r][c] == -1):
-                return
-            visited.add((r,c))
-            q.append([r, c])
-            
-        # loop through the rooms and append all the empty rooms to the queue
+            if (r < 0 or r==ROWS or c < 0 or c == COL or rooms[r][c]== -1 or (r,c) in visit):
+                return 
+            visit.add((r,c))
+            q.append((r,c))
+
         for r in range(ROWS):
-            for c in range(COLS):
+            for c in range(COL):
                 if rooms[r][c] == 0:
-                    q.append([r, c])
-                    visited.add((r, c))
-        # Initialize the distance to the closest gate to 0
+                    q.append((r,c))
+                    visit.add((r,c))
         dist = 0
-
-        # Now, loop through the q and pop and the position
-        # Use the position to update its neighbor
-
         while q:
             for i in range(len(q)):
                 r, c = q.popleft()
                 rooms[r][c] = dist
-                # Perform a BSF to update the rooms 
                 addRoom(r + 1, c)
                 addRoom(r - 1, c)
-                addRoom(r, c + 1)
-                addRoom(r, c - 1)
+                addRoom(r ,c + 1)
+                addRoom(r ,c - 1)
             dist += 1
-        
         return rooms
 
 s = Solution()
 
-print(s.awall_and_gates([[2147483647,-1,0,2147483647],[2147483647,2147483647,2147483647,-1]]))
+print(s.wallsAndGates([[2147483647,-1,0,2147483647],[2147483647,2147483647,2147483647,-1],[2147483647,-1,2147483647,-1],[0,-1,2147483647,2147483647]]))
