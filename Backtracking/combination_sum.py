2367631772dafd732a242894165f77c3dbfb2446
alvannwanorim <alvannwanorim@gmail.com>
2022-12-20 23:40:37 +0100

    Combination Sum

--- Backtracking/combination_sum.py
+++ Backtracking/combination_sum.py
@@ -10,16 +10,17 @@ class Solution:
             if total == target:
                 res.append(curr.copy())
                 return 
-            if total > target:
+            if total > target or i>= len(candidates):
                 return 
             curr.append(candidates[i])
-            dfs(i + 1, curr, total + candidates[i])
+            dfs(i, curr, total + candidates[i])
 
             curr.pop()
 
-            dfs(i + 1, curr, total + candidates[i])
+            dfs(i + 1, curr, total)
 
         dfs(0,[], 0)
         return res
 sol = Solution()
 print(sol.combinationSum([1,2,3,4,5],7))
+print(sol.combinationSum([2,3,6,7],7))
