eaf8a5c0b11c50d337a86b1fe26bdb426f84c8f0
alvannwanorim <alvannwanorim@gmail.com>
2022-12-20 15:39:01 +0100

    Subset

--- Backtracking/subsets.py
+++ Backtracking/subsets.py
@@ -3,23 +3,18 @@ from typing import List
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        subset=[]
         res = []
-
-        subset =[]
-
         def dfs(i):
-            if i >= len(nums):
+            if i >=len(nums):
                 res.append(subset.copy())
                 return
             subset.append(nums[i])
+            
             dfs(i + 1)
-            print(i)
-            print(subset)
-            # remove nums[i] from dfs
             subset.pop()
             dfs(i + 1)
         dfs(0)
-
         return res
 sol = Solution()
-print(sol.subsets([1,2,3,4]))
+print(sol.subsets([1,2,]))
