1a58c9edb0274125aaf922e761bc12196399a12d
alvannwanorim <alvannwanorim@gmail.com>
2022-12-20 23:20:02 +0100

    Subset II

--- Backtracking/subsets_ii.py
+++ Backtracking/subsets_ii.py
@@ -5,21 +5,21 @@ from typing import List
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
       res = []
-      nums.sort()
+    
       def backtrack(i, subset):
-        if i == len(nums):
-            res.append(subset[::])
-            return 
+        if i >= len(nums):
+          res.append(subset.copy())
+          return
         subset.append(nums[i])
         backtrack(i + 1, subset)
-
         subset.pop()
 
-        while i + 1 < len(nums) and nums[i] == nums[i + 1]:
-            i += 1
-        backtrack( i + 1, subset)
-
-      backtrack(0, [])
+        while i +1 < len(nums) and nums[i] == nums[i + 1]:
+          i += 1
+        backtrack(i + 1, subset)
+      
+      backtrack(0,[])
+      
       return res
 sol = Solution()
 print(sol.subsets([1,2,2]))
