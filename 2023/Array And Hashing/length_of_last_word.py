28c64c00836d662f5869dd8b7ad98a66be77d9dc
alvannwanorim <alvannwanorim@gmail.com>
2023-03-27 09:50:11 +0100

    Length of last word

--- 2023/Array And Hashing/length_of_last_word.py
+++ 2023/Array And Hashing/length_of_last_word.py
@@ -4,5 +4,20 @@ class Solution:
         return len(s.split()[-1])
 
 
+class Solution2:
+
+    def lastWord(self, s: str) -> int:
+        i, length = len(s) - 1, 0
+
+        while s[i] == " ":
+            i -= 1
+        while i > 0 and s[i] != " ":
+            length += 1
+            i -= 1
+        return length
+
+
 sol = Solution()
-print(sol.lastWord('Hello Word  '))
+sol2 = Solution2()
+print(sol.lastWord('Hello'))
+print(sol2.lastWord('Hello Word  '))
