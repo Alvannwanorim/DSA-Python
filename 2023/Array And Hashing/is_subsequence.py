78ca32dfdd906cbbc55ce8bc710d81fb1bebde06
alvannwanorim <alvannwanorim@gmail.com>
2023-03-27 08:48:25 +0100

    Is sequence

---
+++ 2023/Array And Hashing/is_subsequence.py
@@ -0,0 +1,15 @@
+class Solution:
+
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+
+        return i == len(s)
+
+
+sol = Solution()
+print(sol.isSubsequence('acb', 'adbec'))
