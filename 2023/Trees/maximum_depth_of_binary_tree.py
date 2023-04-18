4908ed9717fb03817eabd4b39a853782479d4925
alvannwanorim <alvannwanorim@gmail.com>
2023-04-18 13:12:35 +0100

    maximum depth of binary tree

---
+++ 2023/Trees/maximum_depth_of_binary_tree.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val, left=None, right=None) -> None:
+        self.val = val
+        self.left = left 
+        self.right = right 
+
+class Solution:
+    def max_depth_of_Binary_tree(self, root: Optional[TreeNode])->int:
+        max_depth = 0 
+        stack = [[root, 0]]
+
+        while stack:
+            cur, depth = stack.pop()
+            max_depth = max(max_depth, depth)
+            if cur.left: stack.append([cur.left, depth + 1])
+            if cur.right: stack.append([cur.right, depth + 1])
+        
+        return max_depth 
+    
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.left.right.left= TreeNode(6)
+sol = Solution()
+print(sol.max_depth_of_Binary_tree(root))
