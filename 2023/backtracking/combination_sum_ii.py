1da2e062574a5aa666d6fc3874d72b7900b35593
alvannwanorim <alvannwanorim@gmail.com>
2023-04-24 10:13:03 +0100

    Combination sum II

--- 2023/backtracking/combination_sum_ii.py
+++ 2023/backtracking/combination_sum_ii.py
@@ -1,25 +1,22 @@
 from typing import List
 class Solution:
     def combination_sum(self, candidates:List[int], target: int)->List[List[int]]:
+        candidates.sort()
         res = []
 
         def backtrack(pos, cur, target):
             if target == 0:
                 res.append(cur.copy())
+                return
+            if target <0:
                 return 
-            
-            if target < 0:
-                return 
-            
             prev = -1 
-
             for i in range(pos, len(candidates)):
-                if candidates[i] == prev:
+                if prev == candidates[i]:
                     continue
                 cur.append(candidates[i])
-                backtrack(i +1, cur, target - candidates[i])
+                backtrack(i+1, cur, target - candidates[i])
                 cur.pop()
-                prev =candidates[i]
         backtrack(0, [], target)
         return res
 sol = Solution()
