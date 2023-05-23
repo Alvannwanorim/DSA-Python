e072c4ab483f3f528c34f2328c75f4f517bfb8b4
alvannwanorim <alvannwanorim@gmail.com>
2023-05-23 14:44:35 +0100

    Daily tempearture

---
+++ 2023/stack/daily_temperature.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def dailyTemperature(self,temperatures: List[int]):
+        stack = []
+        res = [0] * len(temperatures)
+        
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackIn = stack.pop()
+                res.append((i - stackIn))
+            stack.append([t, i])
+        
+        return res
+sol = Solution()
+print(sol.dailyTemperature([73,72.74,76,70]))
+             
