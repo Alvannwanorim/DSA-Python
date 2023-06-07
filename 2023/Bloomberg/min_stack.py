812471939bf49bd1f59fd0966b8b451643c4d109
alvannwanorim <alvannwanorim@gmail.com>
2023-06-07 22:12:03 +0100

    design min stack

--- 2023/Bloomberg/min_stack.py
+++ 2023/Bloomberg/min_stack.py
@@ -6,27 +6,27 @@ class MinStack:
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        val = min(val, self.min_stack[0] if self.min_stack else val)
+        val = min(val, self.min_stack[-1] if self.min_stack else val)
         self.min_stack.append(val)
         
-
     def pop(self) -> None:
         self.stack.pop()
         self.min_stack.pop()
         
-
     def top(self) -> int:
         return self.stack[-1]
         
-
     def getMin(self) -> int:
         return self.min_stack[-1]
 
 sol = MinStack()
-print(sol.push(-2))
+print(sol.push(2))
 print(sol.push(0))
-print(sol.push(-3))
+print(sol.push(3))
+print(sol.push(0))
 print(sol.getMin())
 print(sol.pop())
+print(sol.getMin())
 print(sol.pop())
 print(sol.getMin())
+
